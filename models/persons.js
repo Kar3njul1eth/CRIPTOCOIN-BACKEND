9b35416a84209c3d8a8836e83df8db4f1690c01b
jersson <jersson.m@outlook.com>
2020-11-21 12:08:57 -0500

    Se elimina relacion para la tabla persons y se elimina la bd del proyecto.

--- models/persons.js
+++ models/persons.js
@@ -2,9 +2,7 @@ const persons = require("../schemas/persons");
 
 const model = (sequelize) => {
   const personsModel = sequelize.define(persons.name, persons.attributes);
-  personsModel.associate = (models) => {
-  personsModel.hasMany(models.persons);
-  };
+  
   return personsModel;
 };
 
